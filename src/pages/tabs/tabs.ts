bcfee8fd54583039ad5d3c3c4bda0486f9da902b
Supavit Panyafang <supavitp59@email.nu.ac.th>
2019-02-15 17:19:45 +0700

    Add row col in page about

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
